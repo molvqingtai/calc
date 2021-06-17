561562590265020d65bedc026a9e364769b9e508
molvqingtai <molvqingtai@gmail.com>
2021-06-17 23:33:46 +0800

    style: fix eslint error

--- src/index.ts
+++ src/index.ts
@@ -16,94 +16,105 @@
 //   (base: number): number =>
 //     number * base
 
-type Formula = (
+type Operator = (
   ...args: number[]
 ) => (
   process: (number: number) => number
 ) => (ending: (number: number) => number) => number
 
-const plus: Formula =
+const plus: Operator =
   (...args) =>
-  (process) =>
-  (ending) =>
+  process =>
+  ending =>
     ending(
       args.reduce((acc, cur, index) =>
         index === 1 ? process(acc) + process(cur) : acc + process(cur)
       )
     )
 
-const minus: Formula =
+const minus: Operator =
   (...args) =>
-  (process) =>
-  (ending) =>
+  process =>
+  ending =>
     ending(
       args.reduce((acc, cur, index) =>
         index === 1 ? process(acc) - process(cur) : acc - process(cur)
       )
     )
 
-const divide: Formula =
+const times: Operator =
   (...args) =>
-  (process) =>
-  (ending) =>
-    ending(
-      args.reduce((acc, cur, index) =>
-        index === 1 ? process(acc) / process(cur) : acc / process(cur)
-      )
-    )
-const times: Formula =
-  (...args) =>
-  (process) =>
-  (ending) =>
+  process =>
+  ending =>
     ending(
       args.reduce((acc, cur, index) =>
         index === 1 ? process(acc) * process(cur) : acc * process(cur)
       )
     )
 
+const divide: Operator =
+  (...args) =>
+  process =>
+  ending =>
+    ending(
+      args.reduce((acc, cur, index) =>
+        index === 1 ? process(acc) / process(cur) : acc / process(cur)
+      )
+    )
+// const power: Operator =
+//   (...args) =>
+//   (process) =>
+//   (ending) =>
+//     ending(
+//       args.reduce((acc, cur, index) =>
+//         index === 1 ? process(acc) ** process(cur) : acc ** process(cur)
+//       )
+//     )
 type Process = (number: number) => (number: number) => number
 const process: Process = (factor: number) => (number: number) => {
   return Number.isInteger(number)
     ? number * factor
-    : +(number * factor).toPrecision(number.toString().length - 1)
+    : +(number * factor).toPrecision(+number.toString().length - 1)
 }
 
 type Ending = (number: number) => (number: number) => number
-const ending: Ending = (divisor) => (number) => {
+const ending: Ending = divisor => number => {
   return Number.isInteger(number)
     ? number / divisor
-    : +(number / divisor).toPrecision(number.toString().length - 1)
+    : +(number / divisor).toPrecision(+number.toString().length - 1)
 }
 
 type Handler = (
-  formula: Formula
+  Operator: Operator
 ) => (process: Process) => (ending: Ending) => (...args: number[]) => number
-const handler: Handler = (formula) => (process) => (ending) => {
+const handler: Handler = Operator => process => ending => {
   return (...args) => {
     const base =
       10 **
       Math.max(
-        ...args.map((number) => number.toString().split('.')[1]?.length ?? 0)
+        ...args.map(number => number.toString().split('.')[1]?.length ?? 0)
       )
-    return formula(...args)(process(base))(ending(base))
+    return Operator(...args)(process(base))(ending(base))
   }
 }
 
-const calc = (...args: number[]): number => {
+const calc = (symbols: TemplateStringsArray, ...values: number[]): number => {
   const runs = new Map<string, Function>([
     ['+', handler(plus)(process)(ending)],
     ['-', handler(minus)(process)(ending)],
     ['*', handler(times)(process)(ending)],
-    ['/', handler(divide)(process)((_) => (_) => _)]
+    ['/', handler(divide)(process)(_ => _ => _)]
+    // ['**', handler(power)(process)(ending)]
   ])
 
   // return runs.get('*')?.(...args) // 35.41, 100
   // return runs.get('-')?.(...args)
   // return runs.get('+')?.(...args)
-  return runs.get('/')?.(...args)
+  // TODO: parse
+  return runs.get('+')?.()
 }
 
-const res = calc(0.1, 10)
+const res = calc`${0.1}+${0.2}+${0.3}`
 console.log(res)
 
 export default calc
